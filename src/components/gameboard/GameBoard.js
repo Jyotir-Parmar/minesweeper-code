aec245dc5653ff698f0a1b0e2281de18acdb7c02
Jyotir Parmar <jparmar@fortinet.com>
2021-02-05 16:47:19 +0530

    fixed winning bug

--- src/components/gameboard/GameBoard.js
+++ src/components/gameboard/GameBoard.js
@@ -8,9 +8,7 @@ import { GAME_STATUS } from '../../constants/constant';
 
 const GameBoard = memo(() => {
     const [grid, setGame] = useState([]);
-    const { config, dispatch, visitedCells } = useContext(gameConfig);
-
-    console.log(config, " ", visitedCells, " ", dispatch);
+    const { config, dispatch, visitedCells, gameStatus } = useContext(gameConfig);
 
     useEffect(() => {
         const newGrid = countBombsNearCells(getInitialGameBoard(config))
@@ -18,18 +16,19 @@ const GameBoard = memo(() => {
     }, []);
 
     useEffect(() => {
-        if (visitedCells === config.gridSize.m * config.gridSize.n) {
+        if (visitedCells === (config.gridSize.m * config.gridSize.n) - config.bombInTheGame) {
             dispatch(createGameStateAction(GAME_STATE_CHANGED, GAME_STATUS.GAME_WON));
         }
     }, [visitedCells]);
 
     const processVisitedCell = (cell) => {
-        console.log("processVisitedCell");
-        if (cell === -1) { //Lost the game
-            dispatch(createGameStateAction(GAME_STATE_CHANGED, GAME_STATUS.GAME_LOST));
-            return;
+        if (gameStatus === GAME_STATUS.GAME_IN_PROGRESS) {
+            if (cell === -1) { //Lost the game
+                dispatch(createGameStateAction(GAME_STATE_CHANGED, GAME_STATUS.GAME_LOST));
+                return;
+            }
+            dispatch(createGameStateAction(VISITED_CELL, null));
         }
-        dispatch(createGameStateAction(VISITED_CELL, null));
     }
 
     return <section className="gameBoard margin-top-20 fadeEffect">
